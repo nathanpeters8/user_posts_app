44f275ff1cf326091ae4c7fa7e6849976350b912
Nathan Peters <peters.nate8@gmail.com>
2024-03-12 16:49:44 -0600

    create post and user models, and migrate the migration files into database

---
+++ db/migrate/20240312224234_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      #attributes
+      t.string :username
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      
+      t.timestamps
+    end
+  end
+end
